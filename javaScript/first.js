84a331185b5913759c0d73b813557264285401fd
Harshvar25 <harshvardhansingh.bais2021@vitstudent.ac.in>
2023-06-20 23:49:20 +0530

    icon color changed

--- javaScript/first.js
+++ javaScript/first.js
@@ -1,7 +1,23 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "70%";
+  document.getElementById("mySidenav").style.width = "70%";
+  document.getElementById("myImg").src = "./images/Active.png";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("myImg").src = "./images/notActive.png";
+}
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    moreText.style.display = "inline";
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+}
