8b469092d3dec0fa3e583a7bb65f21b21c2f51ca
Harshvar25 <harshvardhansingh.bais2021@vitstudent.ac.in>
2023-06-23 14:18:51 +0530

    animations added to buttons and cards

--- lifestyle-card/lifestyle-js/lifestyle.js
+++ lifestyle-card/lifestyle-js/lifestyle.js
@@ -1,9 +1,9 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "70%";
-    document.getElementById("myImg").src = ".../images/Active.png";
+    document.getElementById("myImg").src = "../images/Active.png";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("myImg").src = ".../images/notActive.png";
+    document.getElementById("myImg").src = "../images/notActive.png";
   }
